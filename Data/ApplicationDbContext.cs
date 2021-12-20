f3bde31aa34847c991ce126acee5fbd9a18b25d9
Edward Son <90419532+nahyukee@users.noreply.github.com>
2021-12-20 12:49:12 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MoviesWeb.Models;
+
+namespace MoviesWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { 
+        
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
